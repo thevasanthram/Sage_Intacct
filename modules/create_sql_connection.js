6dcdf38d3f7756587efab1697ca103963ad6eef9
thevasanthram <vasanthram227@gmail.com>
2024-06-11 12:20:36 +0530

    sage_auto_update

--- modules/create_sql_connection.js
+++ modules/create_sql_connection.js
@@ -7,8 +7,8 @@ async function create_sql_connection() {
     user: "pinnacleadmin",
     password: "PiTestBi01",
     server: "pinnaclemep.database.windows.net",
-    // database: "bi_play_ground_update",
-    database: "sage_intacct",
+    database: "bi_play_ground_update",
+    // database: "sage_intacct",
     options: {
       encrypt: true, // Use this option for SSL encryption
       requestTimeout: 48 * 60 * 60 * 1000, // 60 seconds (adjust as needed)
